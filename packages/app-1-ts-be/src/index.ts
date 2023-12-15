4067bc944c3d35486c70a30b51c40d02d9ddc1da
Gustavo Mahecha <gmahechas@outlook.com>
2023-12-14 23:36:20 -0500

    husky

--- packages/app-1-ts-be/src/index.ts
+++ packages/app-1-ts-be/src/index.ts
@@ -4,7 +4,7 @@ const start = async () => {
 	try {
 		app.listen(30000, () => {
 			console.log(`environment:`);
-			console.log(`app is runnig on: http://localhos:${30000}`);
+			console.log(`app is runnig on: http://localhost:${30000}`);
 		});
 	} catch (error) {
 	}
