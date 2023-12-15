33ac0056a55c54a2954db0c85c502df6d68ff0dc
Gustavo Mahecha <gmahechas@outlook.com>
2023-12-14 23:17:18 -0500

    express

---
+++ packages/app-1-ts-be/src/routes/v1/auth.routes.ts
@@ -0,0 +1,9 @@
+import { createExpressRouter } from '@gmahechas/common-express-ts-be';
+
+const router = createExpressRouter();
+
+router.get('/', async (request, response, next) => {
+	response.status(200).send({ data: 'ok from auth' });
+});
+
+export const authRoutes = router.use('/auth', router);
