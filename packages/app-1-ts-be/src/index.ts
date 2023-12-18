62b5c26adf0d05ecf6d24312765a03c08005d8e2
Gustavo Mahecha <gmahechas@outlook.com>
2023-12-17 21:27:43 -0500

    eslint

--- packages/app-1-ts-be/src/index.ts
+++ packages/app-1-ts-be/src/index.ts
@@ -3,7 +3,7 @@ import { app } from './app';
 const start = async () => {
 	try {
 		app.listen(30000, () => {
-			console.log('environment:');
+			console.log(`environment:`);
 			console.log(`app is runnig on: http://localhos:${30000}`);
 		});
 	} catch (error) {
