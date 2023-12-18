4671318d056047b9f3a51049366b7748a289b33e
Gustavo Mahecha <gmahechas@outlook.com>
2023-12-17 21:36:48 -0500

    eslint

--- packages/common/common-express-ts-be/src/adapters/express.ts
+++ packages/common/common-express-ts-be/src/adapters/express.ts
@@ -4,9 +4,9 @@ export const createExpressApp = (): express.Express => {
 	const app = express();
 	app.disable('x-powered-by');
 	return app;
-}
+};
 
 export const createExpressRouter = (): express.Router => {
 	const router = express.Router(); 
 	return router;
-}
+};
