e6afd79fbe8b3241f36b1cb1c3b2566db1a839f0
Gustavo Mahecha <gmahechas@outlook.com>
2023-12-18 20:12:05 -0500

    prettier

--- packages/app-1-ts-be/src/index.ts
+++ packages/app-1-ts-be/src/index.ts
@@ -3,7 +3,7 @@ import { app } from './app';
 const start = async (): Promise<void> => {
 	try {
 		app.listen(30000, () => {
-			console.log('environment:');
+			console.log(`environment:`);
 			console.log(`app is runnig on: http://localhos:${30000}`);
 		});
 	} catch (error) {
